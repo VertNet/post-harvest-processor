bf48bb1db6c16760b177d71b3b4f46118845635e
tucotuco <tuco@berkeley.edu>
2020-11-28 18:29:09 -0300

    Updated GCS cleaner to use Google Cloud Storage client.

--- lib/clean_GCS_folder.py
+++ lib/clean_GCS_folder.py
@@ -16,77 +16,22 @@
 
 # Adapted from check_harvest_folder_GCS.py
 # Adapted from get_harvest_folders.py
+# Updated to Google Cloud Storage Client
 
 __author__ = "John Wieczorek"
 __contributors__ = "John Wieczorek"
-__copyright__ = "Copyright 2018 vertnet.org"
-__this_file__ = "clean_GCS_folder.py"
-__revision_date__ = "2018-09-25T20:41-03:00"
+__copyright__ = "Copyright 2020 vertnet.org"
+__this_file__ = "clean_GCS_folder2.py"
+__revision_date__ = "2020-11-28T18:24-03:00"
 __version__ = "%s %s" % (__this_file__, __revision_date__)
 
-# googleapis has an argparser, which will be invoked when the CloudStorage instance is
-# initiated. We must use that argparser and add our argument processing to it.
-from googleapis.GoogleAPI import parser
-from googleapis import CloudStorage as CS
-
-def get_file_count_from_GCS_folder(cs):
-    '''
-       Get a count of the files in a bucket on GCS.
-    '''
-    if cs is None:
-        print 'No Google Cloud Storage dispatcher given in %s.' % __version__
-    bucket = cs.bucket()
-    if bucket is None:
-        print 'No Google Cloud Storage bucket given in %s.' % __version__
-    
-    resp = cs.list_bucket()
-#    resp = cs.list_bucket(maxResults=10)
-    itemcount = 0
-    while resp is not None:
-#    if resp is not None:
-        if 'items' in resp:
-            itemcount += len(resp['items'])
-        # Check if there is a page token signifying more items in bucket
-        if 'nextPageToken' in resp:
-            # Get next page of items
-            resp = cs.list_bucket(pageToken=resp['nextPageToken'])
-        else:
-        	# No more items
-            resp = None
-    return itemcount
-
-def get_file_list_from_GCS_folder(cs):
-    '''
-       Get a list of the files in a bucket on GCS.
-    '''
-    if cs is None:
-        print 'No Google Cloud Storage dispatcher given in %s.' % __version__
-    bucket = cs.bucket()
-    if bucket is None:
-        print 'No Google Cloud Storage bucket given in %s.' % __version__
-    
-    # Make a list of files in the harvest folder
-    file_list = []
-    resp = cs.list_bucket()
-#    resp = cs.list_bucket(maxResults=10)
-    while resp is not None:
-#    if resp is not None:
-        if 'items' in resp:
-            for item in resp['items']:
-                entry = { 'name':item['name'], 'date':item['timeCreated'] }
-                file_list.append( entry )
-        # Check if there is a page token signifying more items in bucket
-        if 'nextPageToken' in resp:
-            # Get next page of items
-            resp = cs.list_bucket(pageToken=resp['nextPageToken'])
-        else:
-        	# No more items
-            resp = None
-    return file_list
+import argparse
+from datetime import datetime
+from google.cloud import storage
 
 def _getoptions():
     ''' Parse command line options and return them.'''
-    # Use imported parser rather than create a new one
+    parser = argparse.ArgumentParser()
     help = 'Earliest file date to keep (e.g., 2018-06-23; required)'
     parser.add_argument("-d","--date", help=help, required=True)
     help = 'Google Cloud Storage bucket name (e.g., vn-downloads2; required)'
@@ -113,40 +58,37 @@ def main():
         s += ' -d 2018-06-23'
         s += ' -b vn-downloads2'
         s += ' -l True'
-        print s
+        print(s)
+        return
+
+    # Input earliest date to keep must be a a W3C datetime
+    try:
+        delete_before_date=datetime.fromisoformat(options.date)
+    except ValueError as e:
+        print(e)
         return
 
     # Create a CloudStorage Manager to be able to access Google Cloud Storage based on
     # the credentials stored in dl_cred.
-    cs = CS.CloudStorage( {'bucket_name':options.bucket} )
-
-    # Create a list of folders on Google Cloud Storage to process
-    downloadfiles = get_file_list_from_GCS_folder(cs)
-
-    if downloadfiles is None:
-        print 'No files found in bucket in %s.' % cs.bucket()
-        return
-
-    # Make list of files to remove. There is a string match with date, so files with
-    # date less than that given will be removed unless an extact datetime is given
-    # (e.g., 2018-09-04T16:57:27.797Z)
-    remove_file_list = []
-    for file in downloadfiles:
-        if file['date'] <= options.date:
-            remove_file_list.append(file)
-            print 'toss date: %s file: %s' % (file['date'], file['name'])
-
-    # Remove files from remove list
-    print '%s files to remove from %s.' % (len(remove_file_list), cs.bucket())
-    j = 0
-    if options.list_only is None or options.list_only <> 'False':
-        for file in remove_file_list:
-            cs.delete_object(file['name'])
-            j += 1
-    print '%s files removed.' % j
-
-    remainingfilecount = get_file_count_from_GCS_folder(cs)
-    print '%s files remain in %s.' % (remainingfilecount, cs.bucket())
+    # cs = CS.CloudStorage( {'bucket_name':options.bucket} )
+    cs = storage.Client()
+    try:
+      bucket = cs.get_bucket(options.bucket)
+    except google.cloud.exceptions.NotFound:
+      print("Sorry, the bucket %s does not exist!" % options.bucket)
+    all_blobs_list = cs.list_blobs(bucket)
+    blob_count=len(list(all_blobs_list))
+    print("Before: Bucket %s has %s files." % (bucket, blob_count))
+    remove_list=[]
+    all_blobs_list = cs.list_blobs(bucket)
+    for blob in all_blobs_list:
+        if blob.time_created.replace(tzinfo=None)<delete_before_date:
+          remove_list.append(blob)
+    print("%s files to remove from before %s." % (len(remove_list),options.date))
+    bucket.delete_blobs(remove_list)
+    all_blobs_list = cs.list_blobs(bucket)
+    blob_count=len(list(all_blobs_list))
+    print("After: Bucket %s has %s files." % (bucket, blob_count))
 
 if __name__ == "__main__":
     main()
